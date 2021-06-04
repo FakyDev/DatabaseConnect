2f134731ac2e9ec5bb7355b303b0739e9202a412
FakyZDev <74829542+FakyZDev@users.noreply.github.com>
2021-06-04 12:44:34 +0400

    Delete autoload.php

--- autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-//To autoload classes
-spl_autoload_register(function($className) {
-
-	$className = str_replace("\\", DIRECTORY_SEPARATOR, $className);
-	require_once $_SERVER['DOCUMENT_ROOT'] . '/class/' . $className . '.php';
-
-});
-?>
